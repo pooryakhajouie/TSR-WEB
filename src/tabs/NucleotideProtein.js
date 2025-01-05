2cd9016e1285b996037629366dd0f598340894b7
Krishna Rauniyar <C00551157@louisiana.edu>
2025-01-05 13:40:50 -0600

    changes to 9 files

--- src/tabs/NucleotideProtein.js
+++ src/tabs/NucleotideProtein.js
@@ -3,6 +3,22 @@ import './TSR.css';
 import proteinImage from '../images/tsr-method.png';
 
 const NucleotideProtein = () => {
+  const commandStyle = {
+    marginRight: '10px', // Space between command and comment
+    fontWeight: 'bold'   // Optional: Make the command part bold for clarity
+  };
+
+  const commentStyle = {
+    fontStyle: 'italic',
+    color: '#888'  // Light gray color for the comment
+  };
+
+  // Style for the wrapper to align command and comment
+  const lineStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '8px' // Space between lines
+  };
   return (
     <div className="tsr-method-page">
       <aside className="toc">
@@ -10,6 +26,7 @@ const NucleotideProtein = () => {
           <ul>
             <li><a href="#abstract">Abstract</a></li>
             <li><a href="#tutorial">Tutorial</a></li>
+            <li><a href="#slurm">Slurm Guide</a></li>
             <li><a href="#source-code">Source Code</a></li>
           </ul>
         </nav>
@@ -62,7 +79,7 @@ const NucleotideProtein = () => {
               <span className="step-title">Create a virtual environment and activate it:</span>
               <div className="code-block">
                 <code>
-                  python3 -m venv tsrenv<br />
+                  python -m venv tsrenv<br />
                   source tsrenv/bin/activate  # Mac/Linux<br />
                   tsrenv\Scripts\activate  # Windows
                 </code>
@@ -80,14 +97,27 @@ const NucleotideProtein = () => {
           <h3 className="step-title">Usage</h3>
           <p>Once installed, you can use the following command to generate keys and triplets files.</p>
 
-          <h4 className="step-title">Generate Keys and Triplets</h4>
+          <h4 className="step-title">Generate Cross Key Input File</h4>
+          <p>First, use <i>drug_protein_3A.py</i> to calculate interactions between drugs and proteins within 3 Angstroms. It will generate a csv file which will be the input for the cross key generation.</p>
           <div className="code-block">
-            <code>
-              python cross_key.py -p drug_protein_cross.csv -o output_dir
-            </code>
+            <code>python drug_protein_3A.py -p pdb_download_path -c input_file.csv -l drug_atom_lexical_txt.csv -o drug_protein_cross.csv</code>
           </div>
-          <p>This command will store all the results in <strong>output_dir</strong> directory.</p>
-
+          <h4 className="step-title">Required input files:</h4>
+          <ul>
+            <li>
+              <strong>input_file.csv:</strong> Contains list of protein names (For example - sample_details_p53_dna.csv)
+            </li>
+            <li>
+              <strong>drug_atom_lexical_txt.csv:</strong> Contains atom sequence numbers
+            </li>
+          </ul>
+          <h4 className="step-title">Required input files:</h4>
+          <ol>
+            <li>Create directory 'pdb_download_path' if it doesn't exist</li>
+            <li>Download PDB files from RCSB.org</li>
+            <li>Calculate interactions within 3 Angstroms</li>
+            <li>Generate drug_protein_cross.csv with interaction details</li>
+          </ol>
           <h4 className="step-title">CSV file information as input (drug_protein_cross.csv)</h4>
           <div className="code-block">
             <code>
@@ -131,7 +161,20 @@ const NucleotideProtein = () => {
 
           <p>The csv input file includes information for both Nucleotide (drug) and Protein. The first three columns contains the drug name, drug sequence number, and X, Y, Z coordinates and the following three columns contains the protein name, protein sequence number, and X, Y, Z coordinates and the last columns is the distance between these drug and protein. You can find the csv file in the github link below.</p>
 
+          <h4 className="step-title">Generate Keys and Triplets</h4>
+          <p>After generating the drug_protein_cross.csv file, use <i>cross_key.py</i> to generate keys and triplet files:</p>
+          <div className="code-block">
+            <code>python cross_key.py -p drug_protein_cross.csv -o output_dir</code>
+          </div>
+
           <h4 className="step-title">Results</h4>
+          <p>The analysis produces two types of results:</p>
+          <h2 className="step-title">Drug-Protein Interactions (drug_protein_3A.py)</h2>
+          <ul>
+            <li>Generates drug_protein_cross.csv containing all interactions within 3 Angstroms</li>
+            <li>Includes detailed atomic information and distances for each interaction</li>
+          </ul>
+          <h2 className="step-title">CrossKeys (cross_key.py)</h2>
           <p>CrossKeys and triplets will be generated between:
             <ul>
               <li><strong>One drug and two protein atoms</strong></li>
@@ -150,6 +193,81 @@ const NucleotideProtein = () => {
               <li>Cannot make keys for 9_7B4G_B_DC having only 2 atoms.</li>
             </ul>
           </p>
+        </section>
+
+        {/* Slurm Guide Section with New Styles */}
+        <section id="slurm" className="section-tsr tutorial-section">
+          <h2 className="tutorial-title">Slurm Guide</h2>
+
+          <p>A basic Slurm script to submit a job looks like this:</p>
+
+          <h4 className="step-title">Slurm Script</h4>
+          <div className="code-block">
+            <code>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --job-name=my_hpc_job</span>
+                <span style={commentStyle}># Job name</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --output=result.out</span>
+                <span style={commentStyle}># Standard output file</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --error=result.err</span>
+                <span style={commentStyle}># Standard error file</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --ntasks=4</span>
+                <span style={commentStyle}># Number of tasks (e.g., processes)</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --time=01:00:00</span>
+                <span style={commentStyle}># Maximum runtime (HH:MM:SS)</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>#SBATCH --partition=general</span>
+                <span style={commentStyle}># Partition (queue) name</span>
+              </p>
+              <p style={lineStyle}>
+                <span style={commandStyle}>python batch_script.py</span>
+                <span style={commentStyle}># The python script file to run.</span>
+              </p>
+            </code>
+          </div>
+          <h4 className="step-title">Run the batch file</h4>
+          <div className="code-block">
+            <code>
+              sbatch batch_file.sbatch
+            </code>
+          </div>
+          {/* Example Section */}
+          <h3 className="step-title">Examples</h3>
+
+          <h4 className="step-title">Cross Key Generation</h4>
+          <p>We need to create an sbatch file that that run the cross key generation.</p>
+          <div className="code-block">
+            <code>
+              #!/bin/bash<br />
+              #SBATCH -p workq<br />
+              #SBATCH -n 64 <br />
+              #SBATCH -t 72:00:00 <br />
+              #SBATCH -A loni_tsr_4 <br />
+              #SBATCH -N 1 <br />
+              #SBATCH -J cross_key <br />
+              #SBATCH -o output_cross_key.out <br />
+              #SBATCH -e error_cross_key.err <br /><br />
+
+              git clone https://github.com/KrishnaRauniyar/Nucleotide-Protein.git <br />
+              cd Nucleotide-Protein <br />
+              python3 -m venv myenv <br />
+              source myenv/bin/activate <br />
+              pip install --upgrade pip <br />
+              pip install -r requirements.txt <br />
+              <br /><br />
+              python3 drug_protein_3A.py -p pdb_downloaded -c sample_details_p53_dna.csv -l drug_atom_lexical_txt.csv -o drug_protein_cross.csv && \<br />
+              python3 cross_key.py -p drug_protein_cross.csv -o cross_key_results
+            </code>
+          </div>
         </section>
 
         <section id="source-code" className="section-tsr source-code-section">
